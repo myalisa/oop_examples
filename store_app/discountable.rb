5a3409cc7a3b8f8f8ddec7f7593789a97545bcfd
myalisa <myalisamiro@gmail.com>
2019-08-23 10:17:42 -0500

    Namespace Update StoreFront

--- store_app/discountable.rb
+++ store_app/discountable.rb
@@ -1,6 +1,8 @@
-module Discountable
-  def discount_item
-    puts "This item is 20 percent off"
-    # code to discount item
+module StoreFront
+  module Discountable
+    def discount_item
+      puts "This item is 20 percent off"
+      # code to discount item
+    end
   end
 end
--- store_app/item.rb
+++ store_app/item.rb
@@ -1,16 +1,18 @@
-class Item
-  attr_reader :name, :color, :price
-  
+module StoreFront
+  class Item
+    attr_reader :name, :color, :price
+    
 
-  def initialize(input_options)
-    @name = input_options[:name]
-    @color = input_options[:color]
-    @price = input_options[:price]
-  
+    def initialize(input_options)
+      @name = input_options[:name]
+      @color = input_options[:color]
+      @price = input_options[:price]
+    
+    end
+
+    def print_info
+      puts "#{name} is #{color} and costs $#{price}."
+    end
+
   end
-
-  def print_info
-    puts "#{name} is #{color} and costs $#{price}."
-  end
-
 end
